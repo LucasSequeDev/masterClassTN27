b777a84197256fbec7f1a3c999895f1fced54854
lucas.sequeira <lucas.sequeira>
2022-01-14 10:39:43 -0300

    Importancion de los barrios,clientes y pedidos de mi base de datos

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // Aqui comienza todo!
+const fs = require('fs')
+
+const BARRIO_DB_STRING = fs.readFileSync('./BaseDatos/barrios.json', 'utf-8')
+const BARRIOS_DB = JSON.parse(BARRIO_DB_STRING)
+
+const CLIENTES_DB_STRING = fs.readFileSync('./BaseDatos/clientes.json', 'utf-8')
+const CLIENTES_DB = JSON.parse(CLIENTES_DB_STRING)
+
+const PEDIDOS_DB_STRING = fs.readFileSync('./BaseDatos/pedidos.json', 'utf-8')
+const PEDIDOS_DB = JSON.parse(PEDIDOS_DB_STRING)
+
 const miApp = {
-    barrios: [],
-    clientes: [],
-    pedidos: [],
+    barrios: BARRIOS_DB,
+    clientes: CLIENTES_DB,
+    pedidos: PEDIDOS_DB,
 }
 
 module.exports = miApp
